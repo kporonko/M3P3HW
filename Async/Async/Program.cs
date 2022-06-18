780cfdae7db37b21c73e0f8bfa94df1b1573c516
KeeYoo <poronko2045@gmail.com>
2022-06-18 14:13:58 +0300

    Start commit

---
+++ Async/Async/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Async
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
